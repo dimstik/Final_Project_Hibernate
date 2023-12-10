72c3de9ebb929d164d3db9c9fddcfa4b7abe059b
Dima Pavlov <dima.p96@mail.ru>
2023-12-10 13:47:17 +0300

    Compare the speed of retrieving the same data from MySQL and Redis

--- src/main/java/com/javarush/domain/Main.java
+++ src/main/java/com/javarush/domain/Main.java
@@ -37,6 +37,21 @@ public class Main {
         List<City> allCities = main.fetchData(main);
         List<CityCountry> preparedData = main.transformData(allCities);
         main.pushToRedis(preparedData);
+
+        main.sessionFactory.getCurrentSession().close();
+        List<Integer> ids = List.of(3, 2545, 123, 4, 189, 89, 3458, 1189, 10, 102);
+
+        long startRedis = System.currentTimeMillis();
+        main.testRedisData(ids);
+        long stopRedis = System.currentTimeMillis();
+
+        long startMysql = System.currentTimeMillis();
+        main.testMysqlData(ids);
+        long stopMysql = System.currentTimeMillis();
+
+        System.out.printf("%s:\t%d ms\n", "Redis", (stopRedis - startRedis));
+        System.out.printf("%s:\t%d ms\n", "MySQL", (stopMysql - startMysql));
+
         main.shutdown();
     }
 
